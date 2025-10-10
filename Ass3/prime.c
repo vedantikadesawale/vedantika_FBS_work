61ad5ae6358850bd97b3c4fa992a2dcce2f3be3e
vedantikadesawale <vedantika2806@gmail.com>
2025-10-10 22:22:32 +0530

    Update prime.c

--- Ass3/prime.c
+++ Ass3/prime.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
 
 void main() {
-    int n = 7;  
-    int i = 2;
+   int n = 7; 
+    int i=2;  
 
     if (n <= 1) {
         printf("Not Prime\n");
+        return;
     }
 
-    while (i <= n / 2) {
+    while (i <= n/2) {
         if (n % i == 0) {
-            printf("Not Prime\n");   
+            printf("Not Prime\n");
+             return;   
         }
         i++;
     }
     printf("Prime\n");
 
 }
+
